362e8084aaf9961f2a7d443a42b4af5264768daa
Luke McAuley <lukemcauley7@gmail.com>
2022-02-07 23:04:13 +0000

    Basic hello world on QEMU

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,23 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-#[panic_handler]
-2fn panic(_info: &PanicInfo) -> ! {
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    "Welcome to McAuley OS!"
+    .as_bytes()
+    .iter()
+    .flat_map(|bt| [*bt, 0xf as u8])
+    .enumerate()
+    .for_each(|(i, byte)| unsafe {
+        *vga_buffer.offset(i as isize) = byte;
+    });
     loop{}
 }
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
